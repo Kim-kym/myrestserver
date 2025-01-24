3f88ffe3666715e506f9d819a6547061b1306359
Kim-kym <msgim725@gmail.com>
2025-01-24 16:04:16 +0900

    Todo delete $ update

--- MyRestServer/src/main/java/learnbyteaching/todos/controllers/TodoApiController.java
+++ MyRestServer/src/main/java/learnbyteaching/todos/controllers/TodoApiController.java
@@ -1,13 +1,20 @@
 package learnbyteaching.todos.controllers;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import learnbyteaching.todos.repositories.dao.TodoRepository;
@@ -37,5 +44,60 @@ public class TodoApiController {
 		Optional<TodoItem> todo = todoRepository.findById(id);
 		return todo.map(ResponseEntity::ok)
 				.orElseGet(() -> ResponseEntity.notFound().build());
+	}
+	
+	//	keyword 파라미터 전달 받아서 title을 대상으로 검색 -> 목록 
+	@GetMapping("/search")
+	public ResponseEntity<List<TodoItem>> searchTodos(
+			@RequestParam("keyword") String keyword) {
+		List<TodoItem> foundTodos = 
+				todoRepository.findAll().stream()
+					.filter(todo -> todo.getTitle()
+										.toLowerCase()
+										.contains(keyword.toLowerCase()))
+					.collect(Collectors.toList());
+		return ResponseEntity.ok(foundTodos);
+	}
+	
+	//	새로운 TodoItem 생성
+	@PostMapping
+	public ResponseEntity<TodoItem> createTodo(
+			@RequestBody TodoItem todoItem
+			) {
+		TodoItem savedTodo = todoRepository.save(todoItem);
+//		return ResponseEntity.status(HttpStatus.CREATED)
+//				.header("Location", "/api/todos/" + savedTodo.getId())
+//				.body(savedTodo);
+		URI location = 
+				URI.create("api/todos/" + savedTodo.getId());
+		return ResponseEntity.created(location).body(savedTodo);
+	}
+	
+	//	기존 TodoItem 수정 
+	@PutMapping("/{id}") // /api/todos/{id}
+	public ResponseEntity<TodoItem> updateTodo(
+			@PathVariable Long id,
+			@RequestBody TodoItem updatedTodo) {
+		return todoRepository.findById(id)
+				.map(todo -> {
+					todo.setTitle(updatedTodo.getTitle());
+					todo.setCompleted(updatedTodo.isCompleted());
+					TodoItem savedTodo = todoRepository.save(todo);
+					return ResponseEntity.ok(savedTodo);
+				})
+				.orElseGet(() -> ResponseEntity.notFound().build());
+	}
+	
+	//	기존 TodoItem 삭제 
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteTodo(
+			@PathVariable Long id) {
+		Optional<TodoItem> existingTodo = 
+				todoRepository.findById(id);
+		if (!existingTodo.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		todoRepository.deleteById(id);
+		return ResponseEntity.ok().<Void>build();
 	}
 }
